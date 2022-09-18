f486dbe48a3e018629ea1e5f80c93913ab25e557
Felix <felixbeumer03@gmail.com>
2022-09-18 15:08:28 +0200

    Added full Pause screen

--- src/main/states/states/Pause.java
+++ src/main/states/states/Pause.java
@@ -12,16 +12,19 @@ import java.awt.*;
 public class Pause extends GameState {
 
     private Button[] button;
+    private Font title;
     public Pause(GameStateManager gsm) {
         super(gsm);
 
+        title = new Font("EquipmentPro", Font.PLAIN, 90);
+
         button = new Button[3];
 
-        button[0] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,270), "Resume");
+        button[0] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,200), "Resume");
         button[0].setCenter(true);
-        button[1] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,370), "Options");
+        button[1] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,300), "Options");
         button[1].setCenter(true);
-        button[2] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,470), "Main Menu");
+        button[2] = new Button(new main.graphics.Image("ui/GrayLargeButton6.png"), new Vector2f(100,400), "Main Menu");
         button[2].setCenter(true);
     }
 
@@ -57,6 +60,10 @@ public class Pause extends GameState {
     @Override
     public void render(Graphics2D g2) {
         if (!gsm.isState(GameStateManager.OPTIONS)) {
+            g2.setFont(title);
+            g2.setColor(new Color(213, 213, 213));;
+            g2.drawString("Pause", GameStateManager.getXForCenteredFrameText("Pause", g2), 100);
+
             for (Button b : button) {
                 b.render(g2);
             }
